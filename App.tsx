f8640561182abad99ce63c869f4eb73258329abc
Abhirup <abhidatta146@gmail.com>
2022-10-15 08:57:47 +0530

    navigation libs installation and home,shortlisted screens

--- App.tsx
+++ App.tsx
@@ -1,17 +1,44 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  Text,
-} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'; 
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import HomeScreen from './src/screens/Home';
+import ShortListedScreen from './src/screens/Shortlisted';
+
+export type RootStackParamList = {
+  Home: undefined;
+  ShortListed: undefined;
+}
+
+const Tab  = createBottomTabNavigator<RootStackParamList>();
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Text>
-        Hello
-      </Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={({route})=>({
+        tabBarIcon:({focused, color})=>{
+          let iconName;
+          if(route.name === 'Home'){
+            iconName = "home";
+          }
+          else{
+            iconName = "bookmarks-outline";
+          }
+          return <BottomIcon name={iconName} color={color}/>
+        },
+        tabBarActiveTintColor: 'tomato', 
+        tabBarInactiveTintColor:'gray'})}>
+        <Tab.Screen name="Home" component={HomeScreen}/>
+        <Tab.Screen name="ShortListed" component={ShortListedScreen} options={{ tabBarBadge: 3 }}/>
+      </Tab.Navigator>  
+    </NavigationContainer>
   );
 };
 
 export default App;
+
+type BottomIconType = {
+  name: string;
+  color: string;
+}
+const BottomIcon = ({name, color}:BottomIconType)=> <Ionicons name={name} color={color} size={25}/>;
---
+++ src/screens/Shortlisted.tsx
@@ -0,0 +1,11 @@
+import { View,Text } from 'react-native';
+
+const ShortListed = ()=>{
+    return (
+      <View>
+        <Text>ShortListed</Text>
+      </View>
+    )
+  }
+
+export default ShortListed;
