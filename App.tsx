4983870569d1f9ac3b90d33fd2db677a8ae91c34
Abhirup <abhidatta146@gmail.com>
2022-10-15 08:13:25 +0530

    initial RN setup and cleanup

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>
+        Hello
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
